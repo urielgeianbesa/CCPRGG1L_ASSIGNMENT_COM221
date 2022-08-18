37b4b16c5cf2285187df348b5474c5f6abbd0a0f
urielgeianbesa <58388385+urielgeianbesa@users.noreply.github.com>
2022-08-18 15:26:11 +0800

    Add files via upload
    I have uploaded my Assignment

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+public class Assignment#1 App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    } 
+    System.out.println(HELLO, GUYS);
+
+
+
+}
+
